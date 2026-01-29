76f25c2c6b5f022977636800d388a6ef48a217ce
mkrafiul13453 <mkrafiul13453@gamil.com>
2026-01-29 21:09:46 +0600

    for task is done

---
+++ for.js
@@ -0,0 +1,65 @@
+for(let i=1; i<=60; i++){
+    console.log("I will invest at least 6 hrs every single day for next 60 days!");
+}
+
+
+
+
+
+for(i=61; i<=100; i++){
+    if (i%2===0){
+        console.log(i);
+    }
+}
+
+
+
+
+
+
+for(i=78; i<=98; i++){
+    if (i%2!==0){
+        console.log(i);
+    }
+}
+
+
+
+
+sum=0;
+for(i=91; i<=129; i++){
+    if (i%2!==0){
+        sum+=i;
+    }
+}
+
+console.log(sum);
+
+
+
+
+sum2=0;
+for(i=51; i<=85; i++){
+    if (i%2===0){
+        sum2+=i;
+    }
+}
+
+    console.log(sum2);
+
+
+
+
+
+for(let i=1; i<=10; i++){
+    let multi=i*9;
+    console.log(`9 X ${i} = ${multi}`);
+}
+
+
+
+
+for (let i = 81; i >= 65; i--) {
+    console.log(i);
+}
+

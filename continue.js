34becdf1f0c2f27206d1464b2cc0651a7a795028
mkrafiul13453 <mkrafiul13453@gamil.com>
2026-01-29 20:45:00 +0600

    continue task is done

---
+++ continue.js
@@ -0,0 +1,22 @@
+// for (let i = 1; i <= 40; i++) {
+
+//     if (i % 2 !== 0) {
+//         continue; 
+//     }
+
+//     console.log(i); 
+// }
+
+
+
+
+for(let i=55; i<=85; i++){
+   if(i%2!==0){
+
+    if(i%5===0){
+        continue;
+    }
+
+    console.log(i);
+   }
+}
